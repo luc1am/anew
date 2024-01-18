eb653dccd429d96401642340120d22dd6ab9dd9e
luc1am <90805550+luc1am@users.noreply.github.com>
2024-01-18 14:40:35 -0500

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -8,28 +8,28 @@ let frame;
 
 
 function preload(){
-  video[0] = loadImage("assets/head/Layer 1.png");
-  video[1] = loadImage("assets/head/Layer 2.png");
-  video[2] = loadImage("assets/head/Layer 3.png");
-  video[3] = loadImage("assets/head/Layer 4.png");
-  video[4] = loadImage("assets/head/Layer 5.png");
-  video[5] = loadImage("assets/head/Layer 6.png");
-  video[6] = loadImage("assets/head/Layer 7.png");
-  video[7] = loadImage("assets/head/Layer 8.png");
-  video[8] = loadImage("assets/head/Layer 9.png");
-  video[9] = loadImage("assets/head/Layer 10.png");
-  video[10] = loadImage("assets/head/Layer 11.png");
-  video[11] = loadImage("assets/head/Layer 12.png");
-  video[12] = loadImage("assets/head/Layer 13.png");
-  video[13] = loadImage("assets/head/Layer 14.png");
-  video[14] = loadImage("assets/head/Layer 15.png");
-  video[15] = loadImage("assets/head/Layer 16.png");
-  video[16] = loadImage("assets/head/Layer 17.png");
-  video[17] = loadImage("assets/head/Layer 18.png");
-  video[18] = loadImage("assets/head/Layer 19.png");
-  video[19] = loadImage("assets/head/Layer 20.png");
-  video[20] = loadImage("assets/head/Layer 21.png");
-  video[21] = loadImage("assets/head/Layer 22.png");
+  video[0] = loadImage("assets/head/Layer1.png");
+  video[1] = loadImage("assets/head/Layer2.png");
+  video[2] = loadImage("assets/head/Layer3.png");
+  video[3] = loadImage("assets/head/Layer4.png");
+  video[4] = loadImage("assets/head/Layer5.png");
+  video[5] = loadImage("assets/head/Layer6.png");
+  video[6] = loadImage("assets/head/Layer7.png");
+  video[7] = loadImage("assets/head/Layer8.png");
+  video[8] = loadImage("assets/head/Layer9.png");
+  video[9] = loadImage("assets/head/Layer10.png");
+  video[10] = loadImage("assets/head/Layer11.png");
+  video[11] = loadImage("assets/head/Layer12.png");
+  video[12] = loadImage("assets/head/Layer13.png");
+  video[13] = loadImage("assets/head/Layer14.png");
+  video[14] = loadImage("assets/head/Layer15.png");
+  video[15] = loadImage("assets/head/Layer16.png");
+  video[16] = loadImage("assets/head/Layer17.png");
+  video[17] = loadImage("assets/head/Layer18.png");
+  video[18] = loadImage("assets/head/Layer19.png");
+  video[19] = loadImage("assets/head/Layer20.png");
+  video[20] = loadImage("assets/head/Layer21.png");
+  video[21] = loadImage("assets/head/Layer22.png");
 }
 
 function setup() {
